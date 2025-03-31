92cd28e94a62f61ce4bbadaba78387185d5f819b
Egor Kuznecov <140982839+Egor-Kuznetcov@users.noreply.github.com>
2025-03-31 18:52:51 +0300

    Update main.py
    Added del and gc.collect() for master-password, passwords, etc.

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import re
-import time
 from xor import *
 from TCATO import *
 from hashlib import sha256
 from os import urandom
 import keyboard
+import gc
 
 
 def save(password):
-    module = int.from_bytes(urandom(4), byteorder='big')
+    module = int.from_bytes(urandom(8), byteorder='big')
     random = gen_random(len(password))
     key = random_to_key(random, master_password, module)
     password_en = encrypt(password, key)
@@ -16,6 +16,8 @@ def save(password):
         file.write(str(random) + "\n")
     with open("D://passwords.txt", "a") as file:
         file.write(str(password_en) + " " + str(module) + "\n")
+    del key
+    del module
 
 
 def read_passwords():
@@ -30,7 +32,11 @@ def read_passwords():
         password = password.replace("[", "").split(",")
         password = list(map(int, password))
         module = module.replace("\n", "")
-        passwords.append(decrypt(password, random_to_key(random, master_password, int(module))))
+        password_dec = decrypt(password, random_to_key(random, master_password, int(module)))
+        passwords.append(password_dec)
+        del password_dec
+        del module
+
     return passwords
 
 
@@ -82,8 +88,13 @@ try:
                   "select [index] - select password by index\n"
                   "Press 'p' to auto-type selected password")
         elif "save " in user_choice:
-            password = user_choice.split(" ")[1]
-            save(password)
+            try:
+                password = user_choice.split(" ")[1]
+                save(password)
+                del password
+                gc.collect()
+            except:
+                print("Error")
         elif user_choice == "list":
             with open("C://PasswordManager/randoms.txt", "r") as file:
                 randoms = file.readlines()
@@ -97,63 +108,98 @@ try:
                 password = password.replace("[", "").split(",")
                 password = list(map(int, password))
                 module = module.replace("\n", "")
-                print(str(count) + ") " + decrypt(password, random_to_key(random, master_password, int(module))))
+                password_dec = decrypt(password, random_to_key(random, master_password, int(module)))
+                print(str(count) + ") " + password_dec)
+                del password
+                del module
+                del password_dec
+            del strings
+            del randoms
+            gc.collect()
         elif "delete " in user_choice:
-            index = int(user_choice.split(" ")[1])
-            passwords = read_passwords()
-            passwords.pop(index-1)
-            file1 = open("C://PasswordManager/randoms.txt", "w")
-            file2 = open("D://passwords.txt", "w")
-            for q in passwords:
-                module = int.from_bytes(urandom(4), byteorder='big')
-                random = gen_random(len(q))
-                key = random_to_key(random, master_password, module)
-                password_en = encrypt(q, key)
-                file1.write(str(random) + "\n")
-                file2.write(str(password_en) + " " + str(module) + "\n")
-            file1.close()
-            file2.close()
-            print(f"Password {index} deleted")
+            try:
+                index = int(user_choice.split(" ")[1])
+                passwords = read_passwords()
+                passwords.pop(index-1)
+                file1 = open("C://PasswordManager/randoms.txt", "w")
+                file2 = open("D://passwords.txt", "w")
+                for q in passwords:
+                    module = int.from_bytes(urandom(4), byteorder='big')
+                    random = gen_random(len(q))
+                    key = random_to_key(random, master_password, module)
+                    password_en = encrypt(q, key)
+                    file1.write(str(random) + "\n")
+                    file2.write(str(password_en) + " " + str(module) + "\n")
+                    del module
+                file1.close()
+                file2.close()
+                del q
+                del passwords
+                gc.collect()
+                print(f"Password {index} deleted")
+            except:
+                print("Error")
         elif "edit " in user_choice:
-            index = int(user_choice.split(" ")[1])
-            passwords = read_passwords()
-            print("Enter edited password")
-            passwords[index - 1] = input(">>")
-            file1 = open("C://PasswordManager/randoms.txt", "w")
-            file2 = open("D://passwords.txt", "w")
-            for q in passwords:
-                module = int.from_bytes(urandom(4), byteorder='big')
-                random = gen_random(len(q))
-                key = random_to_key(random, master_password, module)
-                password_en = encrypt(q, key)
-                file1.write(str(random) + "\n")
-                file2.write(str(password_en) + " " + str(module) + "\n")
-            file1.close()
-            file2.close()
-            print(f"Password {index} edited")
+            try:
+                index = int(user_choice.split(" ")[1])
+                passwords = read_passwords()
+                print("Enter edited password")
+                passwords[index - 1] = input(">>")
+                file1 = open("C://PasswordManager/randoms.txt", "w")
+                file2 = open("D://passwords.txt", "w")
+                for q in passwords:
+                    module = int.from_bytes(urandom(4), byteorder='big')
+                    random = gen_random(len(q))
+                    key = random_to_key(random, master_password, module)
+                    password_en = encrypt(q, key)
+                    file1.write(str(random) + "\n")
+                    file2.write(str(password_en) + " " + str(module) + "\n")
+                    del module
+                file1.close()
+                file2.close()
+                del q
+                del passwords
+                gc.collect()
+                print(f"Password {index} edited")
+            except:
+                print("Error")
         elif "select " in user_choice:
-            index = int(user_choice.split(" ")[1])
-            passwords = read_passwords()
-            selected_pass = passwords[index-1]
-            print(f"Password {index} selected")
-            keyboard.wait("p")
-            keyboard.press_and_release("backspace")
-            time.sleep(0.007)
-            two_channel_obfuscation(selected_pass)
+            try:
+                index = int(user_choice.split(" ")[1])
+                passwords = read_passwords()
+                selected_pass = passwords[index-1]
+                print(f"Password {index} selected")
+                keyboard.wait("p")
+                keyboard.press_and_release("backspace")
+                time.sleep(0.007)
+                two_channel_obfuscation(selected_pass)
+                del passwords
+                del selected_pass
+                gc.collect()
+            except:
+                print("Error")
         elif user_choice == "exit":
-            passwords = read_passwords()
-            file1 = open("C://PasswordManager/randoms.txt", "w")
-            file2 = open("D://passwords.txt", "w")
-            for q in passwords:
-                module = int.from_bytes(urandom(4), byteorder='big')
-                random = gen_random(len(q))
-                key = random_to_key(random, master_password, module)
-                password_en = encrypt(q, key)
-                file1.write(str(random) + "\n")
-                file2.write(str(password_en) + " " + str(module) + "\n")
-            file1.close()
-            file2.close()
-            break
+            try:
+                passwords = read_passwords()
+                file1 = open("C://PasswordManager/randoms.txt", "w")
+                file2 = open("D://passwords.txt", "w")
+                for q in passwords:
+                    module = int.from_bytes(urandom(4), byteorder='big')
+                    random = gen_random(len(q))
+                    key = random_to_key(random, master_password, module)
+                    password_en = encrypt(q, key)
+                    file1.write(str(random) + "\n")
+                    file2.write(str(password_en) + " " + str(module) + "\n")
+                    del module
+                file1.close()
+                file2.close()
+                del q
+                del passwords
+                del master_password
+                gc.collect()
+                break
+            except:
+                print("Error")
         else:
             print("Unknown command, use help to show the list of command.")
 except:
